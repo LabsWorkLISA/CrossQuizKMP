e115a3cf4b931350a1b347b31a35a2524c83ce18
etherealSent <123739362+etherealSent@users.noreply.github.com>
2024-02-20 22:20:57 +0300

    Decompose added example works

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,3 +1,12 @@
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
+//import androidx.compose.runtime.remember
+//import androidx.compose.ui.window.ComposeUIViewController
+//import com.arkivanov.decompose.DefaultComponentContext
+//import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+//import navigation.RootComponent
+//
+//fun MainViewController() = ComposeUIViewController {
+//    val root = remember {
+//        RootComponent(DefaultComponentContext(LifecycleRegistry()))
+//    }
+//    App(root)
+//}
